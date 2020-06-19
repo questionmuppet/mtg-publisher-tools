733ba8a9965c067b14447268b14ef6229c1e36c3
Jason Schousboe <questionmuppet@gmail.com>
2020-06-19 00:38:00 -0500

    Extended WP_UnitTestCase

---
+++ test-cases/Mtgtools_UnitTestCase.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+use \Mtgtools\Symbols\Mana_Symbol;
+
+abstract class Mtgtools_UnitTestCase extends WP_UnitTestCase
+{
+    /**
+     * ---------------------
+     *   P R O D U C E R S
+     * ---------------------
+     */
+
+    /**
+     * Get mock Mana_Symbol object
+     */
+    protected function get_mock_symbol( array $args = [] ) : Mana_Symbol
+    {
+        $args = array_merge([
+            'is_valid'       => true,
+            'pattern'        => '{T}',
+            'english_phrase' => 'tap this permanent',
+            'svg_uri'        => 'https://img.scryfall.com/symbology/T.svg',
+        ], $args );
+
+        $symbol = $this->createMock( Mana_Symbol::class );
+
+        $symbol->method('is_valid')->willReturn( $args['is_valid'] );
+        $symbol->method('get_pattern')->willReturn( $args['pattern'] );
+        $symbol->method('get_english_phrase')->willReturn( $args['english_phrase'] );
+        $symbol->method('get_svg_uri')->willReturn( $args['svg_uri'] );
+
+        return $symbol;
+    }
+
+}   // End of class
