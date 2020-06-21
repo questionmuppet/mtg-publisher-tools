179088610acc34eadb3540949806db3be851932a
Jason Schousboe <questionmuppet@gmail.com>
2020-06-21 15:34:30 -0500

    Removed deprecated Http_Request classes

--- src/Exceptions/Http/HttpConnectionException.php
+++ src/Exceptions/Http/HttpConnectionException.php
@@ -10,7 +10,7 @@ namespace Mtgtools\Exceptions\Http;
 // Exit if accessed directly
 defined( 'MTGTOOLS__PATH' ) or die("Don't mess with it!");
 
-class HttpConnectionException extends HttpRequestException
+class HttpConnectionException extends \RuntimeException
 {
 
 }
