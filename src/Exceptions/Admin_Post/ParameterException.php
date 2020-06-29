2b06306c301957541b88d6b3b94d9b4ab687c314
Jason Schousboe <questionmuppet@gmail.com>
2020-06-29 15:54:37 -0500

    Created ParameterException

---
+++ src/Exceptions/Admin_Post/ParameterException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ParameterException
+ * 
+ * Exception thrown when admin-post parameters are missing or malformed
+ */
+
+namespace Mtgtools\Exceptions\Admin_Post;
+
+// Exit if accessed directly
+defined( 'MTGTOOLS__PATH' ) or die("Don't mess with it!");
+
+class ParameterException extends PostHandlerException
+{
+
+}
