1f136ff2be21f82c6d045fff2419812244618367
Jason Schousboe <questionmuppet@gmail.com>
2020-07-03 00:59:56 -0500

    Added http status tracking to PostHandlerException

--- src/Exceptions/Admin_Post/AuthorizationException.php
+++ src/Exceptions/Admin_Post/AuthorizationException.php
@@ -12,5 +12,10 @@ defined( 'MTGTOOLS__PATH' ) or die("Don't mess with it!");
 
 class AuthorizationException extends PostHandlerException
 {
+    /**
+     * Default response status
+     */
+    protected $http_code = 403;
+    protected $http_title = 'Forbidden';
 
-}
+}   // End of class
--- src/Exceptions/Admin_Post/ParameterException.php
+++ src/Exceptions/Admin_Post/ParameterException.php
@@ -12,5 +12,10 @@ defined( 'MTGTOOLS__PATH' ) or die("Don't mess with it!");
 
 class ParameterException extends PostHandlerException
 {
+    /**
+     * Default response status
+     */
+    protected $http_code = 400;
+    protected $http_title = 'Bad Request';
 
-}
+}   // End of class
