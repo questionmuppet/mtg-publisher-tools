0e66badf994a988be2e8057bd976c3dbb9546e62
Jason Schousboe <questionmuppet@gmail.com>
2020-07-02 23:48:55 -0500

    Beginning sketch of Updates module

---
+++ templates/dashboard/tabs/updates.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Updates dashboard tab
+ */
+
+// Exit if accessed directly
+defined( 'MTGTOOLS__PATH' ) or die("Don't mess with it!");
+
+// Check permissions
+current_user_can( 'manage_options' ) or die("Quit 'yer sneakin around!");
+
+?>
+
+<section>
+
+</section>
