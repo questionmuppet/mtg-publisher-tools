00e3950e4e2f737aeb5f43c335b4418b90e32b34
Jason Schousboe <questionmuppet@gmail.com>
2020-07-03 01:10:30 -0500

    Created tests for Redirect_Handler

--- tests/wp/Wp_Tasks/Admin_Post/Redirect_Handler-WpTest.php
+++ tests/wp/Wp_Tasks/Admin_Post/Redirect_Handler-WpTest.php
@@ -1,181 +1,171 @@
 <?php
 declare(strict_types=1);
 
-namespace Mtgtools\Wp_Tasks\Admin_Post
+use Mtgtools\Wp_Tasks\Admin_Post as Base;
+use Mtgtools\Exceptions\Admin_Post\PostHandlerException;
+
+class Redirect_Handler_WPTest extends Mtgtools_UnitTestCase
 {
-    use Mtgtools\Exceptions\Admin_Post\PostHandlerException;
+    /**
+     * WP redirect assertions
+     */
+    use WpRedirectAssertionsTrait;
 
     /**
-     * WordPress mock functions
+     * Action hooks
      */
+    const ACTION       = 'request_fake_data';
+    const PRIVATE_HOOK = 'admin_post_' . self::ACTION;
+    const NOPRIV_HOOK  = 'admin_post_nopriv_' . self::ACTION;
 
     /**
-     * Test class
+     * Responses
      */
-    class Redirect_Handler_WPTest extends \Mtgtools_UnitTestCase
+    const CALLBACK_RESULT = [ 'fake_response' ];
+    const ERROR_MESSAGE = 'Ya done fucked up!';
+    const REDIRECT_URL = 'http://example.org/';
+    const HTTP_STATUS = 302;
+
+    /**
+     * Request processor
+     */
+    private $processor;
+
+    /**
+     * Setup
+     */
+    public function setUp() : void
     {
-        /**
-         * WP redirect assertions
-         */
-        use \WpRedirectAssertionsTrait;
+        parent::setUp();
+        $this->processor = $this->createMock( Base\Admin_Request_Processor::class );
+        $this->register_redirect_handler();
+    }
 
-        /**
-         * Action hooks
-         */
-        const ACTION       = 'request_fake_data';
-        const PRIVATE_HOOK = 'admin_post_' . self::ACTION;
-        const NOPRIV_HOOK  = 'admin_post_nopriv_' . self::ACTION;
+    /**
+     * ---------------------------
+     *   A C T I O N   H O O K S
+     * ---------------------------
+     */
 
-        /**
-         * Responses
-         */
-        const CALLBACK_RESULT = [ 'fake_response' ];
-        const ERROR_MESSAGE = 'Ya done fucked up!';
-        const REDIRECT_URL = 'http://example.org/';
-        const HTTP_STATUS = 302;
+    /**
+     * TEST: Can add hooks
+     */
+    public function testCanAddHooks() : void
+    {
+        $handler = $this->create_handler();
 
-        /**
-         * Request processor
-         */
-        private $processor;
+        $result = $handler->add_hooks();
 
-        /**
-         * Setup
-         */
-        public function setUp() : void
-        {
-            parent::setUp();
-            $this->processor = $this->createMock( Admin_Request_Processor::class );
-            $this->register_redirect_handler();
-        }
+        $this->assertNull( $result );
+    }
 
-        /**
-         * ---------------------------
-         *   A C T I O N   H O O K S
-         * ---------------------------
-         */
+    /**
+     * TEST: Private action hook is registered
+     * 
+     * @depends testCanAddHooks
+     */
+    public function testPrivateHookIsRegistered() : void
+    {
+        $handler = $this->create_handler();
 
-        /**
-         * TEST: Can add hooks
-         */
-        public function testCanAddHooks() : void
-        {
-            $handler = $this->create_handler();
+        $handler->add_hooks();
 
-            $result = $handler->add_hooks();
+        $this->assertTrue(
+            has_action( self::PRIVATE_HOOK ),
+            'Failed to assert that private action hook was registered.'
+        );
+    }
 
-            $this->assertNull( $result );
-        }
+    /**
+     * TEST: Nopriv action hook omitted by default
+     * 
+     * @depends testCanAddHooks
+     */
+    public function testNoprivHookIsOmittedByDefault() : void
+    {
+        $handler = $this->create_handler();
 
-        /**
-         * TEST: Private action hook is registered
-         * 
-         * @depends testCanAddHooks
-         */
-        public function testPrivateHookIsRegistered() : void
-        {
-            $handler = $this->create_handler();
+        $handler->add_hooks();
 
-            $handler->add_hooks();
+        $this->assertFalse(
+            has_action( self::NOPRIV_HOOK ),
+            'Failed to assert that "nopriv" action hook was left unregistered by default.'
+        );
+    }
 
-            $this->assertTrue(
-                has_action( self::PRIVATE_HOOK ),
-                'Failed to assert that private action hook was registered.'
-            );
-        }
+    /**
+     * TEST: Can register nopriv action hook
+     * 
+     * @depends testNoprivHookIsOmittedByDefault
+     */
+    public function testCanRegisterNoprivHook() : void
+    {
+        $handler = $this->create_handler([ 'nopriv' => true ]);
 
-        /**
-         * TEST: Nopriv action hook omitted by default
-         * 
-         * @depends testCanAddHooks
-         */
-        public function testNoprivHookIsOmittedByDefault() : void
-        {
-            $handler = $this->create_handler();
+        $handler->add_hooks();
 
-            $handler->add_hooks();
+        $this->assertTrue(
+            has_action( self::NOPRIV_HOOK ),
+            'Failed to assert that "nopriv" action hook could be registered via constructor args.'
+        );
+    }
 
-            $this->assertFalse(
-                has_action( self::NOPRIV_HOOK ),
-                'Failed to assert that "nopriv" action hook was left unregistered by default.'
-            );
-        }
+    /**
+     * -------------------
+     *   R E S P O N S E
+     * -------------------
+     */
 
-        /**
-         * TEST: Can register nopriv action hook
-         * 
-         * @depends testNoprivHookIsOmittedByDefault
-         */
-        public function testCanRegisterNoprivHook() : void
-        {
-            $handler = $this->create_handler([ 'nopriv' => true ]);
+    /**
+     * TEST: Success state redirects using correct parameters
+     */
+    public function testSuccessStateRedirectsUsingCorrectParameters() : void
+    {
+        $this->processor->method('process_request')->willReturn( self::CALLBACK_RESULT );
+        $handler = $this->create_handler();
 
-            $handler->add_hooks();
-
-            $this->assertTrue(
-                has_action( self::NOPRIV_HOOK ),
-                'Failed to assert that "nopriv" action hook could be registered via constructor args.'
-            );
-        }
-
-        /**
-         * -------------------
-         *   R E S P O N S E
-         * -------------------
-         */
-
-        /**
-         * TEST: Success state redirects using correct parameters
-         */
-        public function testSuccessStateRedirectsUsingCorrectParameters() : void
-        {
-            $this->processor->method('process_request')->willReturn( self::CALLBACK_RESULT );
-            $handler = $this->create_handler();
-
-            try {
-                $handler->process_action();
-            }
-            catch ( \WpRedirectAttemptException $e ) {
-                // Expected exception
-            }
-
-            $this->assertWpRedirectLocationContains( self::REDIRECT_URL );
-            $this->assertWpRedirectStatusEquals( self::HTTP_STATUS );
-        }
-
-        /**
-         * TEST: Error state calls wp_die()
-         */
-        public function testErrorStateWpDies() : void
-        {
-            $msg = "An error was encountered during your malformed request, you ne'er-do-well!";
-            $this->processor->method('process_request')->willThrowException( new PostHandlerException( $msg ) );
-            $handler = $this->create_handler();
-
-            $this->setExpectedException( 'WPDieException', "ne'er-do-well" );
-
+        try {
             $handler->process_action();
         }
-
-        /**
-         * ---------------------
-         *   P R O V I D E R S
-         * ---------------------
-         */
-
-        /**
-         * Create handler
-         */
-        private function create_handler( array $args = [] ) : Redirect_Handler
-        {
-            $args = array_replace([
-                'action' => self::ACTION,
-                'callback' => function() { return []; },
-                'redirect_url' => self::REDIRECT_URL,
-            ], $args );
-            return new Redirect_Handler( $args, $this->processor );
+        catch ( WpRedirectAttemptException $e ) {
+            // Expected exception
         }
 
-    }   // End of class
+        $this->assertWpRedirectLocationContains( self::REDIRECT_URL );
+        $this->assertWpRedirectStatusEquals( self::HTTP_STATUS );
+    }
 
-}   // End of namespace
+    /**
+     * TEST: Error state calls wp_die()
+     */
+    public function testErrorStateCallsWpDie() : void
+    {
+        $msg = "An error was encountered during your malformed request, you ne'er-do-well!";
+        $this->processor->method('process_request')->willThrowException( new PostHandlerException( $msg ) );
+        $handler = $this->create_handler();
+
+        $this->setExpectedException( 'WPDieException', "ne'er-do-well" );
+
+        $handler->process_action();
+    }
+
+    /**
+     * ---------------------
+     *   P R O V I D E R S
+     * ---------------------
+     */
+
+    /**
+     * Create handler
+     */
+    private function create_handler( array $args = [] ) : Base\Redirect_Handler
+    {
+        $args = array_replace([
+            'action' => self::ACTION,
+            'callback' => function() { return []; },
+            'redirect_url' => self::REDIRECT_URL,
+        ], $args );
+        return new Base\Redirect_Handler( $args, $this->processor );
+    }
+
+}   // End of class
