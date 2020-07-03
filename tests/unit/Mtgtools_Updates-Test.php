fca840ccd5345bf0a8caec4b04ed650b939f260d
Jason Schousboe <questionmuppet@gmail.com>
2020-07-03 13:11:51 -0500

    Added code to load updates module in plugin

--- tests/unit/Mtgtools_Updates-Test.php
+++ tests/unit/Mtgtools_Updates-Test.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 use Mtgtools\Mtgtools_Updates;
 use Mtgtools\Symbols\Symbol_Db_Ops;
+use Mtgtools\Interfaces\Mtg_Data_Source;
+use Mtgtools\Wp_Task_Library;
 use Mtgtools\Mtgtools_Dashboard;
 
 class Mtgtools_Updates_Test extends Mtgtools_UnitTestCase
@@ -25,9 +27,9 @@ class Mtgtools_Updates_Test extends Mtgtools_UnitTestCase
     public function setUp() : void
     {
         parent::setUp();
-        $this->db_ops = $this->get_mock_db_ops();
-        $this->source = $this->get_mock_mtg_data_source();
-        $this->wp_tasks = $this->get_mock_tasks_library();
+        $this->db_ops = $this->createMock( Symbol_Db_Ops::class );
+        $this->source = $this->createMock( Mtg_Data_Source::class );
+        $this->wp_tasks = $this->createMock( Wp_Task_Library::class );
         $this->updates = new Mtgtools_Updates( $this->db_ops, $this->source, $this->wp_tasks );
     }
 
@@ -46,6 +48,18 @@ class Mtgtools_Updates_Test extends Mtgtools_UnitTestCase
 
         $this->assertNull( $result );
     }
+    
+    /**
+     * TEST: Can add dashboard tab
+     */
+    public function testCanAddDashTab() : void
+    {
+        $dashboard = $this->createMock( Mtgtools_Dashboard::class );
+        
+        $result = $this->updates->add_dash_tab( $dashboard );
+        
+        $this->assertNull( $result );
+    }
 
     /**
      * TEST: Can print notices
@@ -55,32 +69,6 @@ class Mtgtools_Updates_Test extends Mtgtools_UnitTestCase
         $result = $this->updates->print_notices();
 
         $this->assertNull( $result );
-    }
-
-    /**
-     * TEST: Can add dashboard tab
-     */
-    public function testCanAddDashTab() : void
-    {
-        $dashboard = $this->createMock( Mtgtools_Dashboard::class );
-
-        $result = $this->updates->add_dash_tab( $dashboard );
-
-        $this->assertNull( $result );
-    }
-
-    /**
-     * ---------------------
-     *   P R O D U C E R S
-     * ---------------------
-     */
-
-    /**
-     * Get mock db_ops object
-     */
-    private function get_mock_db_ops() : Symbol_Db_Ops
-    {
-        return $this->createMock( Symbol_Db_Ops::class );
     }
 
 }   // End of class
