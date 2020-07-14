1d3713145bb520c3523e243740578274a2fb47d2
Jason Schousboe <questionmuppet@gmail.com>
2020-07-13 21:46:48 -0500

    Created Scryfall card service

---
+++ src/Scryfall/Services/Scryfall_Cards.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * Scryfall_Cards
+ * 
+ * Fetches card data from Scryfall API
+ */
+
+namespace Mtgtools\Scryfall\Services;
+
+use Mtgtools\Cards;
+use Mtgtools\Exceptions\Api as Exceptions;
+
+// Exit if accessed directly
+defined( 'MTGTOOLS__PATH' ) or die("Don't mess with it!");
+
+class Scryfall_Cards extends Scryfall_Api_Handler
+{
+    /**
+     * Valid card search schemes
+     */
+    private $search_schemes = [
+        'scryfall_id' => ['uuid'],
+        'collector_number' => ['set_code', 'collector_number'],
+        'name' => ['name'],
+    ];
+
+    /**
+     * ---------------
+     *   S E A R C H
+     * ---------------
+     */
+
+    /**
+     * Fetch a single card matching search filters
+     * 
+     * @param array $filter One or more filters conforming to a valid search scheme. Priority is granted to the most specific scheme.
+     * @throws ApiException
+     */
+    public function fetch_card_by_filters( array $filters ) : Cards\Magic_Card
+    {
+        $method = $this->find_search_scheme( $filters );
+        $data = call_user_func( $method, $filters );
+        return $this->create_card( $data );
+    }
+
+    /**
+     * Find highest priority search scheme from a filter set
+     * 
+     * @param array $filters    Search filters provided by client
+     * @return callable         Method to execute to retrieve data using scheme
+     */
+    private function find_search_scheme( array $filters ) : callable
+    {
+        foreach ( $this->search_schemes as $key => $required )
+        {
+            if ( $this->contains_required( $filters, $required ) )
+            {
+                return array( $this, "fetch_card_by_{$key}" );
+            }
+        }
+        throw new Exceptions\ScryfallParameterException(
+            sprintf(
+                "%s tried to search Scryfall for a Magic card using an invalid search scheme. No scheme is defined for the filter set: %s.",
+                get_called_class(),
+                implode( ',', array_keys( $filters ) )
+            )
+        );
+    }
+
+    /**
+     * Check for required keys in an array
+     */
+    private function contains_required( array $params, array $required ) : bool
+    {
+        foreach ( $required as $key )
+        {
+            if ( !isset( $params[ $key ] ) )
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * -------------------------------
+     *   S E A R C H   S C H E M E S
+     * -------------------------------
+     */
+
+    /**
+     * Fetch card by unique Scryfall id
+     * 
+     * @param string $filters['uuid']               (required)
+     */
+    private function fetch_card_by_scryfall_id( array $filters ) : array
+    {
+        $uuid = $filters['uuid'];
+        return $this->get_endpoint([ 'endpoint' => "cards/{$uuid}" ]);
+    }
+    
+    /**
+     * Fetch card by set and collector number
+     * 
+     * @param string $filters['set_code']           (required)
+     * @param string $filters['collector_number']   (required)
+     * @param string $filters['language']           (optional, default English)
+     */
+    private function fetch_card_by_collector_number( array $filters ) : array
+    {
+        $identifiers = array_filter([
+            $filters['set_code'],
+            $filters['collector_number'],
+            $filters['language'] ?? '',
+        ]);
+        return $this->get_endpoint([
+            'endpoint' => "cards/" . implode( '/', $identifiers )
+        ]);
+    }
+
+    /**
+     * Fetch card by name
+     * 
+     * @param string $filters['name']               (required)
+     * @param string $filters['set_code']           (optional, default newest edition)
+     */
+    private function fetch_card_by_name( array $filters ) : array
+    {
+        $name = $filters['name'];
+        $set = $filters['set_code'] ?? '';
+
+        $terms = array_filter([
+            sprintf( '!"%s"', $name ),
+            empty( $set ) ? '' : "e:{$set}",
+        ]);
+        $query = urlencode( implode( ' ', $terms ) );
+        $list = $this->get_list_endpoint( "cards/search?q={$query}" );
+        return $list[0];
+    }
+
+    /**
+     * ---------------------------------
+     *   O B J E C T   C R E A T I O N
+     * ---------------------------------
+     */
+
+    /**
+     * Create a Magic_Card object from response data
+     */
+    private function create_card( array $data ) : Cards\Magic_Card
+    {
+        return new Cards\Magic_Card([
+            'uuid' => $data['id'],
+            'name' => $data['name'],
+            'set_code' => $data['set'],
+            'language' => $data['lang'],
+            'collector_number' => $data['collector_number'],
+            'images' => $this->create_image_uris( $data ),
+        ]);
+    }
+
+    /**
+     * Create Image_Uri objects from response data
+     * 
+     * @return Image_Uri[]
+     */
+    private function create_image_uris( array $data ) : array
+    {
+        $images = [];
+        foreach ( $this->find_image_uris_in_card_data( $data ) as $type => $uri )
+        {
+            $image = new Cards\Image_Uri([
+                'card_uuid' => $data['id'],
+                'uri' => $uri,
+                'type' => $type,
+            ]);
+            $images[ $image->get_type() ] = $image;
+        }
+        return $images;
+    }
+
+    /**
+     * Find image uris in response data
+     * 
+     * @return array Image uris from parent, dfc front-face, or empty array if not found
+     */
+    private function find_image_uris_in_card_data( array $data ) : array
+    {
+        return $data['image_uris']
+            ?? $data['card_faces'][0]['image_uris']
+            ?? [];
+    }
+
+}   // End of class
---
+++ tests/http/Scryfall/Services/Scryfall_Cards-HttpTest.php
@@ -0,0 +1,212 @@
+<?php
+declare(strict_types=1);
+
+use Mtgtools\Scryfall\Services\Scryfall_Cards;
+use Mtgtools\Cards\Magic_Card;
+use Mtgtools\Exceptions\Api as Exceptions;
+
+class Scryfall_Cards_HttpTest extends Mtgtools_UnitTestCase
+{
+    /**
+     * Test-card attributes
+     */
+    const UUID = '3faa8c5e-9e1b-4cee-b322-a033bf33dcbc';
+    const NAME = 'Hymn to Tourach';
+    const SET_CODE = 'ema';
+    const COLLECTOR_NUMBER = '92';
+
+    /**
+     * Alternative set code
+     */
+    const ALT_SET_CODE = 'fem';
+    
+    /**
+     * Languages
+     */
+    const LANG_EN = 'en';
+    const LANG_JA = 'ja';
+
+    /**
+     * Image types
+     */
+    const NUM_TYPES = 6;
+
+    /**
+     * Service class
+     */
+    private $cards;
+
+    /**
+     * Setup
+     */
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->cards = new Scryfall_Cards();
+    }
+
+    /**
+     * ---------------
+     *   S E A R C H
+     * ---------------
+     */
+
+    /**
+     * TEST: Can fetch card by uuid
+     */
+    public function testCanFetchCardByUuid() : Magic_Card
+    {
+        $card = $this->cards->fetch_card_by_filters([ 'uuid' => self::UUID ]);
+
+        $this->assertEquals( self::NAME, $card->get_name() );
+
+        return $card;
+    }
+
+    /**
+     * TEST: Can fetch card by collector number
+     */
+    public function testCanFetchCardByCollectorNumber() : void
+    {
+        $card = $this->cards->fetch_card_by_filters([
+            'set_code' => self::SET_CODE,
+            'collector_number' => self::COLLECTOR_NUMBER,
+        ]);
+
+        $this->assertEquals( self::NAME, $card->get_name() );
+    }
+
+    /**
+     * TEST: Can fetch card by collector number with language
+     * 
+     * @depends testCanFetchCardByCollectorNumber
+     */
+    public function testCanFetchCardByCollectorNumberWithLanguage() : void
+    {
+        $card = $this->cards->fetch_card_by_filters([
+            'set_code' => self::SET_CODE,
+            'collector_number' => self::COLLECTOR_NUMBER,
+            'language' => self::LANG_JA,
+        ]);
+
+        $this->assertEquals( self::NAME, $card->get_name() );
+        $this->assertEquals( self::LANG_JA, $card->get_language() );
+    }
+
+    /**
+     * TEST: Can fetch card by name
+     */
+    public function testCanFetchCardByName() : void
+    {
+        $card = $this->cards->fetch_card_by_filters([ 'name' => self::NAME ]);
+
+        $this->assertEquals( self::NAME, $card->get_name() );
+    }
+
+    /**
+     * TEST: Can fetch card by name with set
+     * 
+     * @depends testCanFetchCardByName
+     */
+    public function testCanFetchCardByNameWithSet() : void
+    {
+        $card = $this->cards->fetch_card_by_filters([
+            'name' => self::NAME,
+            'set_code' => self::ALT_SET_CODE
+        ]);
+
+        $this->assertEquals( self::NAME, $card->get_name() );
+        $this->assertEquals( self::ALT_SET_CODE, $card->get_set_code() );
+    }
+
+    /**
+     * ---------------
+     *   I M A G E S
+     * ---------------
+     */
+
+    /**
+     * Single-faced card contains image uris
+     * 
+     * @depends testCanFetchCardByUuid
+     */
+    public function testSimpleCardContainsImageUris( Magic_Card $card ) : void
+    {
+        $this->assertCount( self::NUM_TYPES, $card->get_images(), 'Failed to assert that a Magic Card returned by Scryfall contained the right number of image uris.' );
+    }
+
+    /**
+     * Double-faced card contains image uris
+     * 
+     * @depends testSimpleCardContainsImageUris
+     * @depends testCanFetchCardByName
+     */
+    public function testDfcCardContainsImageUris() : void
+    {
+        $card = $this->cards->fetch_card_by_filters([ 'name' => 'Delver of Secrets' ]);
+
+        $this->assertCount( self::NUM_TYPES, $card->get_images(), 'Failed to assert that a double-faced card returned by Scryfall contained the right number of image uris.' );
+    }
+
+    /**
+     * -----------------------
+     *   E X C E P T I O N S
+     * -----------------------
+     */
+
+    /**
+     * TEST: Fetching with invalid search scheme throws ScryfallParameterException
+     */
+    public function testFetchingWithInvalidSearchSchemeThrowsScryfallParameterException() : void
+    {
+        $this->expectException( Exceptions\ScryfallParameterException::class );
+
+        $this->cards->fetch_card_by_filters([
+            'irrelevant_key' => 'blah'
+        ]);
+    }
+
+    /**
+     * TEST: Id search with no results throws ApiException
+     * 
+     * @depends testCanFetchCardByUuid
+     */
+    public function testIdSearchWithNoResultsThrowsApiException() : void
+    {
+        $this->expectException( Exceptions\ApiException::class );
+
+        $this->cards->fetch_card_by_filters([
+            'uuid' => 'invalid_scryfall_id_string'
+        ]);
+    }
+
+    /**
+     * TEST: Collector number search with no results throws ApiException
+     * 
+     * @depends testCanFetchCardByCollectorNumber
+     */
+    public function testCollectorNumberSearchWithNoResultsThrowsApiException() : void
+    {
+        $this->expectException( Exceptions\ApiException::class );
+
+        $this->cards->fetch_card_by_filters([
+            'set_code' => 'invalid_set',
+            'collector_number' => 'invalid_number',
+        ]);
+    }
+
+    /**
+     * TEST: Name search with no results throws ApiException
+     * 
+     * @depends testCanFetchCardByName
+     */
+    public function testNameSearchWithNoResultsThrowsApiException() : void
+    {
+        $this->expectException( Exceptions\ApiException::class );
+
+        $this->cards->fetch_card_by_filters([
+            'name' => 'nonexistent card name',
+        ]);
+    }
+
+}   // End of class
