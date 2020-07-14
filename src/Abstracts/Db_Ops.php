84aea673003c7cd99fb826e1315a7508066c7982
Jason Schousboe <questionmuppet@gmail.com>
2020-07-13 21:49:25 -0500

    Created Card_Db_Ops

---
+++ src/Abstracts/Db_Ops.php
@@ -0,0 +1,289 @@
+<?php
+/**
+ * Db_Ops
+ * 
+ * Abstract class for handling database operations on a custom table
+ */
+
+namespace Mtgtools\Abstracts;
+
+use Mtgtools\Exceptions\Db as Exceptions;
+use \wpdb;
+
+// Exit if accessed directly
+defined( 'MTGTOOLS__PATH' ) or die("Don't mess with it!");
+
+abstract class Db_Ops extends Data
+{
+    /**
+     * Db tables
+     */
+    protected $tables = [];
+
+    /**
+     * Valid filter arguments for query
+     */
+    protected $valid_filters = [];
+
+    /**
+     * Database class
+     */
+    private $db;
+
+    /**
+     * Constructor
+     */
+    public function __construct( wpdb $db, array $props = [] )
+    {
+        $this->db = $db;
+        parent::__construct( $props );
+        $this->tables = array_replace( $this->tables, $this->get_prop( 'tables' ) );
+    }
+
+    /**
+     * -----------------
+     *   R E C O R D S
+     * -----------------
+     */
+
+    /**
+     * Update a table row, or create it if it doesn't exist
+     * 
+     * @param string $params['table']
+     * @param array $params['identifiers']
+     * @param array $params['values']
+     * @return bool True if a record was updated or inserted, false if not
+     */
+    protected function save_record( array $params ) : bool
+    {
+        $params = array_replace(
+            [
+                'table' => '',
+                'identifiers' => [],
+                'values' => [],
+            ],
+            $params
+        );
+        $params['table'] = $this->get_table_name( $params['table'] );
+        
+        $filters = [];
+        foreach ( $params['identifiers'] as $key )
+        {
+            $filters[ $key ] = $params['values'][ $key ];
+        }
+        $params['identifiers'] = $filters;
+
+        return $this->record_exists( $params['table'], $params['identifiers'] )
+            ? $this->update_row( $params )
+            : $this->insert_row( $params );
+    }
+
+    /**
+     * Check if a record exists in the db
+     */
+    private function record_exists( string $table, array $identifiers ) : bool
+    {
+        return (bool) $this->db()->get_var(
+            sprintf(
+                "SELECT id FROM %s WHERE %s",
+                sanitize_key( $table ),
+                $this->where_conditions( $identifiers )
+            )
+        );
+    }
+
+    /**
+     * Update row in table
+     * 
+     * @return bool Success status
+     */
+    private function update_row( array $params ) : bool
+    {
+        return (bool) $this->db()->update(
+            $params['table'],
+            $params['values'],
+            $params['identifiers'],     // WHERE clause
+            '%s',                       // Format
+            '%s'                        // WHERE format
+        );
+    }
+
+    /**
+     * Insert row into table
+     * 
+     * @return bool Success status
+     */
+    private function insert_row( array $params ) : bool
+    {
+        return (bool) $this->db()->insert(
+            $params['table'],
+            $params['values'],
+            '%s'    // Format
+        );
+    }
+
+    /**
+     * -------------------------------
+     *   S E A R C H   F I L T E R S
+     * -------------------------------
+     */
+
+    /**
+     * Generate WHERE conditions from filters
+     * 
+     * @param array $filters    One or more "column" => "value" pairs to use as conditions
+     * @param bool $exact       Whether to search for an exact match
+     * @return string           Sanitized expression for use in WHERE statement
+     */
+    protected function where_conditions( array $filters, bool $exact = true ) : string
+    {
+        $conditions = [];
+        foreach ( $filters as $key => $value )
+        {
+            if ( !$this->is_valid_filter( $key ) )
+            {
+                throw new Exceptions\DbException( get_called_class() . " tried to retrieve database rows using an unknown filter key '{$key}'." );
+            }
+            $conditions[] = $this->generate_where_condition([
+                'key' => $key,
+                'value' => $value,
+                'exact' => $exact,
+            ]);
+        }
+        return implode( ' && ', $conditions );
+    }
+    
+    /**
+     * Generate WHERE condition
+     */
+    private function generate_where_condition( array $params ) : string
+    {
+        $params = array_replace([
+            'key' => '',
+            'value' => '',
+            'exact' => true,
+        ], $params );
+
+        return $params['exact']
+            ? $this->prepare_exact_match( $params['key'], $params['value'] )
+            : $this->prepare_fuzzy_match( $params['key'], $params['value'] );
+    }
+
+    /**
+     * Prepare an exact-match conditional
+     */
+    private function prepare_exact_match( string $key, string $value ) : string
+    {
+        $column = sanitize_key( $key );
+        return $this->db()->prepare( "{$column} = %s", $value );
+    }
+
+    /**
+     * Prepare a fuzzy-match conditional
+     */
+    private function prepare_fuzzy_match( string $key, string $value ) : string
+    {
+        $column = sanitize_key( $key );
+        return $this->db()->prepare(
+            "{$column} LIKE %s",
+            '%' . $this->db()->esc_like( $value ) . '%'
+        );
+    }
+
+
+    /**
+     * Check if a query filter is valid
+     */
+    private function is_valid_filter( string $key ) : bool
+    {
+        return in_array( $key, $this->valid_filters );
+    }
+
+    /**
+     * ---------------
+     *   T A B L E S
+     * ---------------
+     */
+
+    /**
+     * Get table name by key
+     */
+    protected function get_table_name( string $key ) : string
+    {
+        if ( !$this->table_defined( $key ) )
+        {
+            throw new \OutOfRangeException(
+                sprintf(
+                    "Attempted a database operation on an undefined table. No table defined in class '%s' with key '%s'.",
+                    get_called_class(),
+                    $key
+                )
+            );
+        }
+        return $this->db()->prefix . $this->tables[ $key ];
+    }
+
+    /**
+     * Check if table is defined
+     */
+    private function table_defined( string $key ) : bool
+    {
+        return array_key_exists( $key, $this->tables );
+    }
+    
+    /**
+     * ---------------------------
+     *   T R A N S A C T I O N S
+     * ---------------------------
+     */
+
+    /**
+     * Start transaction
+     */
+    protected function start_transaction() : void
+    {
+        $this->db()->query( 'SET autocommit = 0;' );
+        $this->db()->query( 'START TRANSACTION;' );
+    }
+
+    /**
+     * Commit transaction
+     */
+    protected function commit_transaction() : void
+    {
+        $this->db()->query( 'COMMIT;' );
+        $this->db()->query( 'SET autocommit = 1;' );
+    }
+
+    /**
+     * Rollback transaction
+     */
+    protected function rollback_transaction() : void
+    {
+        $this->db()->query( 'ROLLBACK;' );
+        $this->db()->query( 'SET autocommit = 1;' );
+    }
+    
+    /**
+     * ---------------------------
+     *   D E P E N D E N C I E S
+     * ---------------------------
+     */
+
+    /**
+     * Get charset collation
+     */
+    protected function get_collate() : string
+    {
+        return $this->db()->get_charset_collate();
+    }
+
+    /**
+     * Get WordPress database class
+     */
+    protected function db() : wpdb
+    {
+        return $this->db;
+    }
+
+}   // End of class
---
+++ tests/db/Cards/Card_Db_Ops-DbTest.php
@@ -0,0 +1,395 @@
+<?php
+declare(strict_types=1);
+
+use Mtgtools\Cards\Card_Db_Ops;
+use Mtgtools\Cards\Magic_Card;
+use Mtgtools\Cards\Image_Uri;
+use Mtgtools\Exceptions\Db as Exceptions;
+
+class Card_Db_Ops_DbTest extends Mtgtools_UnitTestCase
+{
+    /**
+     * Dummy table name
+     */
+    const TEST_TABLE = 'mtgtools_cards_TEST';
+    const IMG_TEST_TABLE = 'mtgtools_images_TEST';
+
+    /**
+     * Mock Magic card attributes
+     */
+    const MOCK_CARD = [
+        'uuid' => 'xxxxx',
+        'name' => 'Stoneforge Mystic',
+        'set_code' => 'WWK',
+        'language' => 'English',
+        'variant' => '',
+        'images' => [],
+    ];
+
+    /**
+     * Mock image uri attributes
+     */
+    const IMAGE_1 = [
+        'card_uuid' => self::MOCK_CARD['uuid'],
+        'type' => 'small',
+        'uri' => 'https://www.example.com/small.png',
+    ];
+    const IMAGE_2 = [
+        'card_uuid' => self::MOCK_CARD['uuid'],
+        'type' => 'large',
+        'uri' => 'https://www.example.com/large.png',
+    ];
+
+    /**
+     * Db_Ops object
+     */
+    private $db_ops;
+
+    /**
+     * Live dependencies
+     */
+    private $wpdb;
+
+    /**
+     * Setup
+     */
+    public function setUp() : void
+    {
+        parent::setUp();
+        global $wpdb;
+        $this->wpdb = $wpdb;
+        $this->db_ops = new Card_Db_Ops(
+            $this->wpdb,
+            [
+                'tables' => [
+                    'cards' => self::TEST_TABLE,
+                    'images' => self::IMG_TEST_TABLE,
+                ],
+            ]
+        );
+        $this->remove_temp_table_filters();
+    }
+    
+    /**
+     * Remove temporary table filters (allows for foreign key constraints)
+     */
+    private function remove_temp_table_filters() : void
+    {
+        remove_filter( 'query', array( $this, '_create_temporary_tables' ) );
+        remove_filter( 'query', array( $this, '_drop_temporary_tables' ) );
+    }
+
+    /**
+     * Teardown
+     */
+    public function tearDown() : void
+    {
+        $tables = [
+            self::IMG_TEST_TABLE,
+            self::TEST_TABLE,
+        ];
+        foreach ( $tables as $table )
+        {
+            $this->wpdb->query(
+                sprintf(
+                    "DROP TABLE IF EXISTS %s",
+                    sanitize_key( $this->wpdb->prefix . $table )
+                )
+            );
+        }
+        parent::tearDown();
+    }
+
+    /**
+     * ---------------------------
+     *   I N S T A L L A T I O N
+     * ---------------------------
+     */
+
+    /**
+     * TEST: Can create tables
+     */
+    public function testCanCreateTables() : void
+    {
+        $success = $this->db_ops->create_tables();
+
+        $this->assertTrue( $success );
+    }
+
+    /**
+     * TEST: Can drop tables
+     * 
+     * @depends testCanCreateTables
+     */
+    public function testCanDropTables() : void
+    {
+        $this->db_ops->create_tables();
+
+        $success = $this->db_ops->drop_tables();
+
+        $this->assertTrue( $success );
+    }
+
+    /**
+     * -----------------------
+     *   C A R D   C A C H E
+     * -----------------------
+     */
+
+    /**
+     * TEST: Can cache new Magic card
+     * 
+     * @depends testCanCreateTables
+     * @return array Newly created row in db
+     */
+    public function testCanCacheNewMagicCard() : array
+    {
+        $this->db_ops->create_tables();
+        $card = $this->get_mock_magic_card();
+
+        $this->db_ops->cache_card_data( $card );
+
+        $row = $this->wpdb->get_row(
+            $this->wpdb->prepare(
+                "SELECT * FROM {$this->get_cards_table()} WHERE uuid = %s",
+                self::MOCK_CARD['uuid']
+            ),
+            ARRAY_A
+        );
+
+        $this->assertIsArray( $row );
+
+        return $row;
+    }
+    
+    /**
+     * TEST: Card has correct attributes in db
+     * 
+     * @depends testCanCacheNewMagicCard
+     */
+    public function testNewlyInsertedCardHasCorrectAttributes( array $row ) : void
+    {
+        $this->assertEquals( self::MOCK_CARD['uuid'], $row['uuid'] );
+        $this->assertEquals( self::MOCK_CARD['name'], $row['name'] );
+        $this->assertEquals( self::MOCK_CARD['set_code'], $row['set_code'] );
+        $this->assertEquals( self::MOCK_CARD['language'], $row['language'] );
+        $this->assertEquals( self::MOCK_CARD['variant'], $row['variant'] );
+    }
+
+    /**
+     * -------------------------
+     *   I M A G E   C A C H E
+     * -------------------------
+     */
+
+    /**
+     * TEST: Can cache a single image uri
+     * 
+     * @depends testCanCacheNewMagicCard
+     * @return array Newly created row in db
+     */
+    public function testCanCacheSingleImageUri() : array
+    {
+        $this->db_ops->create_tables();
+        $card = $this->get_mock_magic_card([ 'images' => $this->get_mock_images() ]);
+
+        $this->db_ops->cache_card_data( $card, 'small' );
+
+        $rows = $this->wpdb->get_results(
+            $this->wpdb->prepare(
+                "SELECT * FROM {$this->get_images_table()} WHERE card_uuid = %s",
+                self::MOCK_CARD['uuid']
+            ),
+            ARRAY_A
+        );
+
+        $this->assertCount( 1, $rows, 'Failed to assert that exactly one image uri was cached when a valid type was provided.' );
+
+        return $rows[0];
+    }
+
+    /**
+     * TEST: New image uri has correct attributes
+     * 
+     * @depends testCanCacheSingleImageUri
+     */
+    public function testNewlyInsertedImageHasCorrectAttributes( array $row ) : void
+    {
+        $this->assertEquals( self::IMAGE_1['card_uuid'], $row['card_uuid'] );
+        $this->assertEquals( self::IMAGE_1['type'], $row['type'] );
+        $this->assertEquals( self::IMAGE_1['uri'], $row['uri'] );
+        $this->assertLessThanOrEqual( time(), $row['cached'], 'Failed to assert that the timestamp on a newly cached image uri is equal to or older than current time.' );
+    }
+
+    /**
+     * TEST: Can cache all image uris
+     * 
+     * @depends testCanCacheSingleImageUri
+     */
+    public function testCanCacheAllImageUris() : void
+    {
+        $this->db_ops->create_tables();
+        $card = $this->get_mock_magic_card([ 'images' => $this->get_mock_images() ]);
+
+        $this->db_ops->cache_card_data( $card );
+
+        $rows = $this->wpdb->get_results(
+            $this->wpdb->prepare(
+                "SELECT * FROM {$this->get_images_table()} WHERE card_uuid = %s",
+                self::MOCK_CARD['uuid']
+            )
+        );
+        $this->assertCount( 2, $rows );
+    }
+
+    /**
+     * -------------
+     *   Q U E R Y
+     * -------------
+     */
+
+    /**
+     * TEST: Query without filters throws DomainException
+     */
+    public function testQueryWithoutFiltersThrowsDomainException() : void
+    {
+        $this->expectException( \DomainException::class );
+
+        $card = $this->db_ops->find_card([]);
+    }
+
+    /**
+     * TEST: Can find card by uuid
+     * 
+     * @depends testNewlyInsertedImageHasCorrectAttributes
+     */
+    public function testCanFindCardByUuid() : void
+    {
+        $this->db_ops->create_tables();
+        $this->db_ops->cache_card_data( $this->get_mock_magic_card() );
+
+        $card = $this->db_ops->find_card([
+            'uuid' => self::MOCK_CARD['uuid']
+        ]);
+        
+        $this->assertInstanceOf( Magic_Card::class, $card );
+    }
+
+    /**
+     * TEST: Can find card by composite key
+     * 
+     * @depends testCanFindCardByUuid
+     */
+    public function testCanFindCardByCompositeKey() : void
+    {
+        $this->db_ops->create_tables();
+        $this->db_ops->cache_card_data( $this->get_mock_magic_card() );
+
+        $card = $this->db_ops->find_card([
+            'name' => self::MOCK_CARD['name'],
+            'set_code' => self::MOCK_CARD['set_code'],
+            'language' => self::MOCK_CARD['language'],
+        ]);
+
+        $this->assertInstanceOf( Magic_Card::class, $card );
+    }
+
+    /**
+     * TEST: Zero-result search throws DbException
+     * 
+     * @depends testCanFindCardByCompositeKey
+     */
+    public function testZeroResultSearchThrowsDbException() : void
+    {
+        $this->db_ops->create_tables();
+        $this->db_ops->cache_card_data( $this->get_mock_magic_card() );
+
+        $this->expectException( Exceptions\DbException::class );
+
+        $card = $this->db_ops->find_card([
+            'uuid' => self::MOCK_CARD['uuid'],
+            'name' => 'Incorrect Card Name',
+        ]);
+    }
+
+    /**
+     * TEST: Extant image data is added to Magic card
+     * 
+     * @depends testCanFindCardByUuid
+     */
+    public function testExtantImagesAddedToMagicCard() : void
+    {
+        $this->db_ops->create_tables();
+        $this->db_ops->cache_card_data(
+            $this->get_mock_magic_card([
+                'images' => $this->get_mock_images()
+            ])
+        );
+
+        $card = $this->db_ops->find_card([ 'uuid' => self::MOCK_CARD['uuid'] ]);
+
+        $this->assertCount( 2, $card->get_images(), 'A Magic_Card returned by a query did not contain the expected number of image uris.' );
+        $this->assertEquals( self::IMAGE_1['uri'], $card->get_image_uri( self::IMAGE_1['type'] ), 'Failed to assert that an image uri returned by a query contained the expected values.' );
+    }
+
+    /**
+     * ---------------------
+     *   P R O V I D E R S
+     * ---------------------
+     */
+
+    /**
+     * Get mock Magic card with image uris
+     */
+    private function get_mock_magic_card( array $props = [] ) : Magic_Card
+    {
+        $props = array_replace( self::MOCK_CARD, $props );
+        $card = $this->createMock( Magic_Card::class );
+        foreach ( $props as $key => $prop )
+        {
+            $card->method( "get_{$key}" )->willReturn( $prop );
+        }
+        return $card;
+    }
+
+    /**
+     * Get mock images
+     */
+    private function get_mock_images() : array
+    {
+        return [
+            self::IMAGE_1['type'] => $this->get_mock_image_uri( self::IMAGE_1 ),
+            self::IMAGE_2['type'] => $this->get_mock_image_uri( self::IMAGE_2 ),
+        ];
+    }
+
+    /**
+     * Get mock image uri
+     */
+    private function get_mock_image_uri( array $props = [] ) : Image_Uri
+    {
+        $image = $this->createMock( Image_Uri::class );
+        foreach ( $props as $key => $prop )
+        {
+            $image->method( "get_{$key}" )->willReturn( $prop );
+        }
+        return $image;
+    }
+
+    /**
+     * Get sanitized cards table
+     */
+    private function get_cards_table() : string
+    {
+        return sanitize_key( $this->wpdb->prefix . self::TEST_TABLE );
+    }
+
+    /**
+     * Get sanitized images table
+     */
+    private function get_images_table() : string
+    {
+        return sanitize_key( $this->wpdb->prefix . self::IMG_TEST_TABLE );
+    }
+
+}   // End of class
