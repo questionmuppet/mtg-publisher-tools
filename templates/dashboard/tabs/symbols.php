fcb25ed0ad22c01b094141b3ebf5c067346bbf41
Jason Schousboe <questionmuppet@gmail.com>
2020-07-24 13:02:22 -0500

    Created mana-symbol quick guide

--- templates/dashboard/tabs/symbols.php
+++ templates/dashboard/tabs/symbols.php
@@ -13,24 +13,48 @@ current_user_can( 'manage_options' ) or die("Quit 'yer sneakin around!");
 
 ?>
 
-<section>
+<section class="mtgtools-flex horiz">
     
-    <div class="mtgtools-flex horiz">
+    <div class="mtgtools-information-panel mtgtools-flex-item">
         
-        <div style="max-width: 600px;" class="mtgtools-flex-item">
-            
-            <p><a>Check for updates</a></p>
-            
-            <p>The following mana symbols are available for use in your articles and themes. Simply wrap whatever text you want to parse in [mana_symbols][/mana_symbols] tags. This should work for any text copied directly from Gatherer or Scryfall.</p>
-            
-        </div>
-    
-        <div class="mtgtools-flex-item">
-        
-            <?php $dashboard->display_table( 'symbol_list' ); ?>
-        
-        </div>
+        <h2>Using Mana Symbols</h2>
 
+        <p>Mana symbols can be included in posts and themes using the <code>[mana_symbols]</code> shortcode. Wrap whatever text you want to parse within the shortcode tags, and WordPress handles the rest. You can also show a single symbol with the alternate shortcode <code>[mana_symbol]</code>.</p>
+
+        <p>By default MTG Publisher Tools pulls its data from Scryfall. Text copied from either Scryfall or Gatherer should be parsable by <code>[mana_symbol]</code> tags. When in doubt, refer to the table on this page for the correct code.</p>
+
+        <?php $examples = [
+            '[mana_symbol key="{E}"]',
+            '[mana_symbols]{T}: Add {G}{G}.[/mana_symbols]',
+            '[mana_symbols]Pay {X}{R}{R}: Deal X damage.[/mana_symbols]',
+        ]; ?>
+        <?php $dashboard->print_simple_table([
+            'columns' => [ 'Example', 'Output' ],
+            'rows' => [
+                [ "<code>{$examples[0]}</code>", do_shortcode( $examples[0] ) ],
+                [ "<code>{$examples[1]}</code>", do_shortcode( $examples[1] ) ],
+                [ "<code>{$examples[2]}</code>", do_shortcode( $examples[2] ) ],
+            ],
+        ]); ?>
+
+        <hr width="50%">
+        
+        <h2>Toolbar Button</h2>
+        
+        <p>Within Classic blocks and the Classic Editor, mana-symbol tags can be inserted using the toolbar. To do this, highlight the text you want to wrap and click the mana symbols button.</p>
+
+        <img
+            src="<?php echo esc_url( MTGTOOLS__ASSETS_URL . 'img/mana-symbol-tags-toolbar.png' ); ?>"
+            alt="Toolbar with mana symbol button selected"
+            class="mtgtools-screenshot"
+        />
+        
+    </div>
+    
+    <div class="mtgtools-flex-item" style="margin-top: 26px;">
+    
+        <?php $dashboard->display_table( 'symbol_list' ); ?>
+    
     </div>
 
 </section>
